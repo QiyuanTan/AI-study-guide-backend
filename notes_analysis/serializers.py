7139bcb1c4772f50f6ecf5c117fbee3894884a4c
Qiyuan Tan <119595547+QiyuanTan@users.noreply.github.com>
2025-05-01 20:52:43 -0700

    initial commit

---
+++ notes_analysis/serializers.py
@@ -0,0 +1,7 @@
+# -*- coding = utf-8 -*-
+# @Time : 2025/5/1 20:50
+# @File : serializers
+
+
+if __name__ == '__main__':
+    pass
---
+++ notes_analysis/urls.py
@@ -0,0 +1,7 @@
+# -*- coding = utf-8 -*-
+# @Time : 2025/5/1 20:50
+# @File : urls
+
+
+if __name__ == '__main__':
+    pass
