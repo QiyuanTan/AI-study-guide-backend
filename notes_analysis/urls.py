145bcb66ab48338788fc653c22b4e6dd156121a8
KabirPrasad <kabir.prasad.2005@gmail.com>
2025-05-15 22:39:13 -0700

    Update urls.py

--- notes_analysis/urls.py
+++ notes_analysis/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet
+from .views import CourseViewSet, NoteViewSet
 
 router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
+router.register(r'note', NoteViewSet, basename='note')
 
 urlpatterns = [
     path('', include(router.urls)),
