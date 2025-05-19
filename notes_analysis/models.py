be755590c27733121cda8385dcc1474b568dbfa3
Qiyuan Tan <119595547+QiyuanTan@users.noreply.github.com>
2025-05-19 09:06:08 -0700

    added models

--- notes_analysis/models.py
+++ notes_analysis/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     name = models.CharField(max_length=200)
-    # Add other fields as needed
+    instructor = models.CharField(max_length=50)
     syllabus = models.FileField(upload_to='syllabi/', null=True, blank=True)
     
     def __str__(self):
         return self.name
+
+class Note(models.Model):
+    course = models.ForeignKey(Course, related_name='notes', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return
